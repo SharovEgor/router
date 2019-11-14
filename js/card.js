71d133a20454a3a3e76e1aecf08ef85cc01ae007
karipnail <45046434+karipnail@users.noreply.github.com>
2019-11-14 21:33:07 +0300

    посмотри что не так я блять не понимаю что за хрень

--- js/card.js
+++ js/card.js
@@ -11,11 +11,41 @@ function init () {
         // В режиме добавления новых путевых точек запрещаем ставить точки поверх объектов карты.
         editorDrawOver: false
     });
+
+    var buttonEditor = new ymaps.control.Button({
+        data: { content: "Режим редактирования" }
+    });
+
+    buttonEditor.events.add("select", function () {
+        /**
+         * Включение режима редактирования.
+         * В качестве опций может быть передан объект с полями:
+         * addWayPoints - разрешает добавление новых путевых точек при клике на карту. Значение по умолчанию: false.
+         * dragWayPoints - разрешает перетаскивание уже существующих путевых точек. Значение по умолчанию: true.
+         * removeWayPoints - разрешает удаление путевых точек при двойном клике по ним. Значение по умолчанию: false.
+         * dragViaPoints - разрешает перетаскивание уже существующих транзитных точек. Значение по умолчанию: true.
+         * removeViaPoints - разрешает удаление транзитных точек при двойном клике по ним. Значение по умолчанию: true.
+         * addMidPoints - разрешает добавление промежуточных транзитных или путевых точек посредством перетаскивания маркера, появляющегося при наведении курсора мыши на активный маршрут. Тип добавляемых точек задается опцией midPointsType. Значение по умолчанию: true
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml#editor
+         */
+        multiRoute.editor.start({
+            addWayPoints: true,
+            removeWayPoints: true
+        });
+    });
+
+    buttonEditor.events.add("deselect", function () {
+        // Выключение режима редактирования.
+        multiRoute.editor.stop();
+    });
+
     // Создаем карту с добавленной на нее кнопкой.
-    var myMap = new ymaps.Map('map', {
+    var myMap = new ymaps.Map('Second__map', {
         center: [56.399625, 36.71120],
         zoom: 7,
-
+        controls: [buttonEditor]
+    }, {
+        buttonMaxWidth: 300
     });
 
     // Добавляем мультимаршрут на карту.
@@ -23,4 +53,5 @@ function init () {
 }
 
 ymaps.ready(init);
+
 
